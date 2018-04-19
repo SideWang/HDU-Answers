3c1ebae2b054186d85b525b25e3c7e2a9042f5d0
Side <31005168+SideWang@users.noreply.github.com>
2018-04-19 22:57:58 +0800

    frst commit to git

---
+++ 1/1/1-1-8.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std;
+
+int main(){
+	int m,n;
+	cin >> m;
+	for(int i = 0;i < m;i++){
+		cin >> n;
+		int sum = 0,num;
+		for(int i = 0;i < n;i++){
+			cin >> num;
+			sum += num;
+		}
+		cout << sum << endl;
+		if(i != (n-1))
+			cout << endl;
+	}
+		
+	return 0;
+}
