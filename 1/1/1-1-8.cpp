1d81a15162a942979fde50deb7e081711b732cf4
Side <31005168+SideWang@users.noreply.github.com>
2018-04-20 15:27:31 +0800

    modify 1.1.8

--- 1/1/1-1-8.cpp
+++ 1/1/1-1-8.cpp
@@ -1,19 +1,30 @@
 #include <iostream> 
 using namespace std;
 
+#define LOCAL 0
+#if LOCAL
+#include <fstream>
+#include <ctime>
+#endif
+
 int main(){
+#if LOCAL
+	fstream cin("datain.txt",fstream::in);
+	fstream cout("dataout.txt",fstream::app);
+	cout << endl << "time:"<< time(0)%1000 << endl;
+#endif
 	int m,n;
 	cin >> m;
-	for(int i = 0;i < m;i++){
+	while(m--){
 		cin >> n;
 		int sum = 0,num;
 		for(int i = 0;i < n;i++){
 			cin >> num;
 			sum += num;
 		}
-		cout << sum << endl;
-		if(i != (n-1))
-			cout << endl;
+		cout << sum;
+		if(m)
+			cout << endl << endl;
 	}
 		
 	return 0;
