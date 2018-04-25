7da81db3bff521c25d0d0f7e699aa3e2acf08232
Side <31005168+SideWang@users.noreply.github.com>
2018-04-26 06:48:47 +0800

    change directory

--- Chapter 1/Section 3/1-3-2.cpp
+++ Chapter 1/Section 3/1-3-2.cpp
@@ -2,15 +2,14 @@
 #include <algorithm>
 #include <string.h> 
 using namespace std;
-#define LOCAL 1
-
+#define LOCAL 0
 struct tim{
 	int s;
 	int e;
 	int flag;
 }t[205];
 
-bool f(tim x, tim y){
+bool f(const tim x,const tim y){
 	if(x.s < y.s){
 		return true;
 	}else if(x.s > y.s){
@@ -28,7 +27,6 @@ int main(){
 #if LOCAL
 	freopen ("datain.txt","r",stdin);
     freopen ("dataout.txt","w",stdout);
-	cout << "#########################" << endl;
 #endif
 	int n;
 	cin >> n;
@@ -43,32 +41,29 @@ int main(){
 			}
 			t[j].s = a;
 			t[j].e = b;
+			t[j].flag = 0;
+			if(t[j].s%2 == 0)
+				t[j].s--;
+			if(t[j].e%2 == 1)
+				t[j].e++;
 		}
 		sort(t,t+m,f);
-		for(int k = 0;k < m;k++){
-			t[k].flag = 0;
-		}
 		int sum = 0;
 		for(int j = 0;j < m;j++){
-			cout << t[j].s << " " << t[j].e << endl;
+//			cout << "#" << t[j].s << " " << t[j].e << endl;
 			if(t[j].flag == 0){
 				t[j].flag = 1;
 				int tmp = t[j].e;
-				for(int k = j+1;k < n;k++){
-					if(t[k].s-tmp == 1){
-						if(tmp%2 == 0){
-							t[k].flag = 1;
-							tmp = t[k].e;
-						}
-					}else if(t[k].s-tmp >= 2){
+				for(int k = j+1;k < m;k++){
+					if(t[k].flag == 0 && t[k].s > tmp){
 						t[k].flag = 1;
 						tmp = t[k].e;
 					}
 				}
-				sum++;				
+				sum++;
 			}
 		}
-		cout << "#" << sum*10 << endl; 
+		cout << sum*10 << endl; 
 	}
 	
 	return 0;
